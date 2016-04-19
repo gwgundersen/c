e4e1b0d2330d0d0c7783b67378d892c08435528e
gwg <ggundersen@gmail.com>
2016-04-18 21:47:31 -0400

    Exercise 1

---
+++ 02-types-ops-exprs/01-ranges.c
@@ -0,0 +1,28 @@
+//
+// ranges
+//
+// Determine ranges of char, short, int, and long variables, both signed and
+// unsigned, by printing appropriate values from standard headers and by direct
+// computation.
+//-----------------------------------------------------------------------------
+
+#include <limits.h>
+#include <stdio.h>
+
+main()
+{
+    printf("RANGES*\n");
+    printf("------\n");
+
+    printf("Signed char:\t%d\t\t\t%d\n", SCHAR_MIN, SCHAR_MAX);
+    printf("Unsigned char:\t%d\t\t\t%d\n", 0, UCHAR_MAX);
+    printf("Signed short:\t%d\t\t\t%d\n", SHRT_MIN, SHRT_MAX);
+    printf("Unsigned short:\t%d\t\t\t%d\n", 0, USHRT_MAX);
+    printf("Signed int:\t%d\t\t%d\n", INT_MIN, INT_MAX);
+    printf("Unsigned int:\t%d\t\t\t%d\n", 0, UINT_MAX);
+    printf("Signed long:\t%ld\t%ld\n", LONG_MIN, LONG_MAX);
+    printf("Unsigned long:\t%d\t\t\t%lu\n", 0, ULONG_MAX);
+
+    printf("\n* 0 is considered positive\n");
+}
+
