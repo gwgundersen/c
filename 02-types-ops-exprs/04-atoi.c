eadece9978067d2b5e9c4250071291435d1d1034
gwg <ggundersen@gmail.com>
2016-04-19 23:39:03 -0400

    Amended comment once I realized what was actually happening.

--- 02-types-ops-exprs/04-atoi.c
+++ 02-types-ops-exprs/04-atoi.c
@@ -23,11 +23,12 @@ int atoi(char s[])
     for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i) {
         // 10 * n to move the virtual decimal place.
         //
-        // (s[i] - '0') gives the numeric value of the character stored in s[i]
-        // because the values '0', '1', ... form a contiguous increasing
-        // sequence.
-        //
-        // In other words, (s[i] - '0') is casting types.
+        // (s[i] - '0') works for all character sets because they all represent
+        // '0', '1', '2', ... in contiguous order AND because they all begin
+        // with the numbers 0, 1, 2, ...
+        // 
+        // For example, let's say the character set numerical value for '0' was
+        // X. Then '3' would be X+3. And ('3' - '0') would be X + 3 - X = 3.
         n = 10 * n + (s[i] - '0');
     }
     return n;
