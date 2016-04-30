7245b7de9a958312e51d921db656a248fa0c1f9a
gwg <ggundersen@gmail.com>
2016-04-30 14:11:51 -0400

    Print formatting for unsigned int

--- 02-types_operations_expressions/01-ranges.c
+++ 02-types_operations_expressions/01-ranges.c
@@ -14,8 +14,8 @@ int main(void)
     printf("Unsigned char:\t%d\t\t\t%d\n", 0, UCHAR_MAX);
     printf("Signed short:\t%d\t\t\t%d\n", SHRT_MIN, SHRT_MAX);
     printf("Unsigned short:\t%d\t\t\t%d\n", 0, USHRT_MAX);
-    printf("Signed int:\t%d\t\t%d\n", INT_MIN, INT_MAX);
-    printf("Unsigned int:\t%d\t\t\t%d\n", 0, UINT_MAX);
+    printf("Signed int:\t%u\t\t%u\n", INT_MIN, INT_MAX);
+    printf("Unsigned int:\t%d\t\t\t%u\n", 0, UINT_MAX);
     printf("Signed long:\t%ld\t%ld\n", LONG_MIN, LONG_MAX);
     printf("Unsigned long:\t%d\t\t\t%lu\n", 0, ULONG_MAX);
 
