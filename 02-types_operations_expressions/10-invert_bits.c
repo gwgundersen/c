b2296f2ff7ab4d12bff7105663b8ca13bf6e18c2
gwg <ggundersen@gmail.com>
2016-04-30 18:18:43 -0400

    Clarified comments

--- 02-types_operations_expressions/10-invert_bits.c
+++ 02-types_operations_expressions/10-invert_bits.c
@@ -25,20 +25,32 @@ unsigned invert(unsigned x, int p, int n)
     // Let z pairwise denote ~x.
     // 0000 0000 0zzz zzzz 0000
     a = ~x & mask_query(p, n);
+
     // xxxx xxxx x000 0000 xxxx
     b = x & ~mask_query(p, n); 
+
+    // xxxx xxxx xzzz zzzz xxxx
     return a | b;
 }
 
 // mask_query: Return mask to query n bits at position p.
+//
+// Example
+// -------
+//
+// Values:
+//   p = 10
+//   n = 7
+//
+// Create mask and shift:
+//   0                        0000 0000 0000 0000
+//   ~0                       1111 1111 1111 1111
+//   ~0 << n                  1111 1111 1000 0000
+//   ~(~0 << n)               0000 0000 0111 1111
+//   ~(~0 << n) << (p+1-n)    0000 0111 1111 0000
+//
 unsigned mask_query(int p, int n)
 {
-    // Example using p = 10, n = 7.
-    //
-    // 0                      0000 0000 0000 0000
-    // ~0                     1111 1111 1111 1111
-    // ~0 << n                1111 1111 1000 0000
-    // ~(~0 << n)             0000 0000 0111 1111
-    // ~(~0 << n) << (p+1-n)  0000 0111 1111 0000
+
     return ~(~0U << n) << (p+1-n);
 }
